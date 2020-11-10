ebda0316bc37e0f1de4b4303fdbf4e085da939a3
Colm Britton <colmjude@gmail.com>
2020-11-10 09:46:17 +0000

    Jon's updated convert func

--- bin/convert_geojson.py
+++ bin/convert_geojson.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 
+import shapely.wkt
 
-def wkt_to_json_geometry(wkt):
-    prefix = "MULTIPOLYGON ((("
-    if not wkt.startswith(prefix):
-        raise ValueError(f"cannot parse wkt: {wkt[0:20]}")
 
-    values = wkt[len(prefix) : -len(")))")]
-    coords = [
-        [float(lon), float(lat)]
-        for lon, lat in (point.split() for point in values.split(","))
-    ]
-
-    result = {
-        "type": "MultiPolygon",
-        "coordinates": [[coords]],
-    }
-
-    return result
+def wkt_to_json_geometry(input_):
+    shape = shapely.wkt.loads(input_)
+    return shapely.geometry.mapping(shape)
