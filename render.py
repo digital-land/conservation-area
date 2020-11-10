fe4a0e0e0289a4d2d0117823c41490fc63607c7c
Colm Britton <colmjude@gmail.com>
2020-11-10 10:06:33 +0000

    Add basic conservation area geography index page

--- render.py
+++ render.py
@@ -28,23 +28,28 @@ def render(path, template, **kwargs):
 
 
 env = setup_jinja()
+index_template = env.get_template("index.html")
 area_template = env.get_template("area.html")
 
 
-def create_geometry_file(row, idx):
-    # temporary ids using row number and resource
-    area_dir = f"docs/{idx}:{row['resource']}"
+def create_geometry_file(area, idx):
+    area_dir = f"docs/{area['id']}"
     if not os.path.exists(area_dir):
         os.mkdir(area_dir)
     try:
-        geojson = wkt_to_json_geometry(row["geometry"])
+        geojson = wkt_to_json_geometry(area["geometry"])
         with open(f"{area_dir}/geometry.geojson", "w") as f:
             json.dump(geojson, f)
     except Exception as e:
         print(e)
 
 
+conservation_areas = []
 for idx, o in enumerate(csv.DictReader(open(dataset_csv)), start=1):
+    o["id"] = f"{idx}:{o['resource']}"  # temporary ids using row number and resource
     create_geometry_file(o, idx)
     del o["geometry"]  # don't need to send to template
+    conservation_areas.append(o)
     render(f"{idx}:{o['resource']}/index.html", area_template, con_area=o)
+
+render("index.html", index_template, conservation_areas=conservation_areas)
